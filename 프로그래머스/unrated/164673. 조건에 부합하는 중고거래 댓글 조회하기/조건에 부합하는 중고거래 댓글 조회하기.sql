c888b987256e744455f9fad1adb882608d3f7d09
naro_Kim <51940808+naro-Kim@users.noreply.github.com>
2023-03-15 12:52:05 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,6 @@
+SELECT a.TITLE, a.BOARD_ID, b.REPLY_ID, b.WRITER_ID, b.CONTENTS, DATE_FORMAT(b.CREATED_DATE, "%Y-%m-%d") 
+as CREATED_DATE from USED_GOODS_Reply as b 
+inner join USED_GOODS_BOARD as a 
+on a.BOARD_ID = b.BOARD_ID 
+where DATE_FORMAT(a.CREATED_DATE, "%Y-%m") = "2022-10" 
+order by b.CREATED_DATE asc, a.TITLE asc;
