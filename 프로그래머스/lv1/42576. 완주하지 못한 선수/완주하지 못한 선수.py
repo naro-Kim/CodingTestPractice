359506cc235d7c1e0952196c6d01bf6693bdeb04
naro_Kim <51940808+naro-Kim@users.noreply.github.com>
2023-04-05 01:09:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.92 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,10 +1,10 @@
 from collections import Counter
 
 def solution(participant, completion):
-    c = Counter(participant)
-    for item in completion:
-        c[item] -= 1
-    return "".join(c.elements())
-
+    return "".join((Counter(participant) - Counter(completion)).elements()) 
     
+    #c = Counter(participant) - Counter(completion) 
+    #return "".join(c.elements() 
+
+    # return "".join(c.most_common()[0][0]) 
     #d = {key: 0 for key in participant}
