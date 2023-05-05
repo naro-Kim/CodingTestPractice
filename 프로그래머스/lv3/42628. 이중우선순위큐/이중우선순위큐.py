e445c0693993fa6f6e027e76162108c0ccd78455
naro_Kim <51940808+naro-Kim@users.noreply.github.com>
2023-05-05 16:46:45 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,15 @@
+import heapq as hq
+
+def solution(op):
+        q = []
+        for i in op:
+            print(q)
+            ins, num = i.strip().split() 
+            if ins == 'I':
+                hq.heappush(q, int(num))
+            elif ins == 'D' and q:
+                if num == '-1':
+                    hq.heappop(q)
+                elif num == '1':
+                    q.pop()
+        return [max(q), min(q)] if q else [0,0]
