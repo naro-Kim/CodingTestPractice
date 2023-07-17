8c5b700dad624f2434d655ddae7251273b5a86ff
naro_Kim <51940808+naro-Kim@users.noreply.github.com>
2023-07-17 16:42:50 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,3 +1,17 @@
-def solution(id_list, report, k):
-    answer = []
-    return answer
+def solution(id_list, report, k): 
+    hack = {id: [] for id in id_list}
+
+    for case in report:
+        a, b = case.split(' ')
+
+        if a not in hack[b]:
+            hack[b].append(a)
+
+    result = [0] * len(id_list)
+    for idx, user in enumerate(id_list):
+        for reported_users in hack.values():
+            if user in reported_users and len(reported_users) >= k:
+                result[idx] += 1
+
+    return result
+
