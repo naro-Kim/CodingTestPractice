ac95f8888804cd033339daca4a0c28f7d98c9452
naro_Kim <51940808+naro-Kim@users.noreply.github.com>
2023-09-03 03:11:59 +0900

    [Silver III] Title: N과 M (6), Time: 136 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.js
@@ -0,0 +1,28 @@
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const [n, m] = input[0].split(' ');
+const arr = input[1].split(' ').map((e) => +e).sort((a, b) => a - b);
+
+function solution(n, m, arr) {
+  const seq = new Array(m).fill(0); 
+  let result = '';
+
+  const recursion = (k, num) => { 
+    if (k >= m) {
+      result += `${seq.join(' ')}\n`; 
+      return;
+    } 
+    for (let i = num; i < n; i++) { 
+        seq[k] = arr[i];
+        recursion(k + 1, i + 1); 
+    }
+  }
+  recursion(0, 0);
+  console.log(result);
+}
+
+solution(n, m, arr);
