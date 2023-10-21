0d62004c68f9542194f2577a293f46ecab409083
naro_Kim <51940808+naro-Kim@users.noreply.github.com>
2023-10-21 18:16:25 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/README.md
+++ 프로그래머스/1/92334. 신고 결과 받기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신고 결과 받기 - 92334 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.12 ms
+메모리: 33.4 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 6일 18:14:4
+2023년 10월 6일 18:16:24
 
 ### 문제 설명
 
--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,18 +1,15 @@
 function solution(id_list, report, k) {
-    const ans = new Array(id_list.length).fill(0)      
-    // create report data list for each id
-    const user = id_list.reduce((acc, id)=> ({...acc, [id]:[]}),{});  
-    
-    // checking report and push data into Object
-    report.forEach((c)=>{
-        const [a, b] = c.split(" ");
-        if(!user[b].includes(a)) user[b].push(a); // push data to send a mail to reporter 
-    }) 
-     
-    for (const u in user) {
-        if (user[u].length >= k) {
-          user[u].forEach((item) => (ans[id_list.indexOf(item)] += 1));
+    let reports = [...new Set(report)].map(a=>{return a.split(' ')});
+    let counts = new Map();
+    for (const bad of reports){
+        counts.set(bad[1],counts.get(bad[1])+1||1)
+    }
+    let good = new Map();
+    for(const report of reports){
+        if(counts.get(report[1])>=k){
+            good.set(report[0],good.get(report[0])+1||1)
         }
     }
-    return ans
+    let answer = id_list.map(a=>good.get(a)||0)
+    return answer;
 }
