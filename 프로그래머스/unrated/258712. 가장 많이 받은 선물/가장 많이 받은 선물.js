d9bc5cedaf59d120f0fd2ab7b15420a0fde33184
naro_Kim <51940808+naro-Kim@users.noreply.github.com>
2024-01-08 14:38:25 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 75.88 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,60 +1,80 @@
-const makeMat = (v, n, e) => { 
-    const mat = Array.from({length: n}, ()=> new Array(n).fill(0));
-    
-    const graph = v.reduce((map, key, idx) => { 
-    map.set(key, idx);
-    return map;
-}, new Map);
-     
-    for(const [a,b] of e){
-        mat[graph.get(a)][graph.get(b)] = mat[graph.get(a)][graph.get(b)] + 1 || 1 ; //선물 주는 사람
-    }
-    return mat
-}
-
-const makeTable = (mat) => {
-    const len = mat.length;
-    const arr = [];
-
-    for (let i = 0; i < len; i++) {
-        let sumRows = mat[i].reduce((acc, cur) => acc + cur, 0); // sum of rows
-        let sumCols = 0; 
-        for (let j = 0; j < len; j++) {
-            sumCols += mat[j][i]; // sum of columns
-        }
-        arr.push(sumRows - sumCols); // difference
-    }
-
-    return arr;
-}
-
-const getGift = (friends, mat, tb) => { 
-    const len = friends.length;
-    const giftArr = new Array(len).fill(0);
-    for(let i=0; i<len; i++){  
-        for(let j=0; j<len; j++){
-            if(mat[i][j]){ 
-                if(mat[i][j] > mat[j][i]){
-                    giftArr[i]++;
-                } else if(mat[i][j] == mat[j][i]){
-                    if(tb[i] > tb[j]) giftArr[i]++;
-                }    
-            } else if (!mat[i][j] && !mat[j][i]) {           
-                if(tb[i] > tb[j]) giftArr[i]++; 
+function initializeRecords(friends) {
+    let giftGiven = {};
+    let giftReceived = {};
+    friends.forEach(friend => {
+        giftGiven[friend] = {};
+        giftReceived[friend] = {};
+        friends.forEach(other => {
+            if (friend !== other) {
+                giftGiven[friend][other] = 0;
+                giftReceived[friend][other] = 0;
             }
-        } 
-    }
-    
-    return Math.max(...giftArr);
+        });
+    });
+    console.log(giftGiven,giftReceived)
+    return { giftGiven, giftReceived };
 }
 
-const solution = (friends, gifts) => {
-    let answer = 0;
-    const len = friends.length;
-    gifts = gifts.map((v)=> v.split(" "));
-    const mat = makeMat(friends, friends.length, gifts);
-    const tb = makeTable(mat); 
-    answer = getGift(friends, mat, tb); 
-    
-    return answer;
+function updateGiftRecords(gifts, giftGiven, giftReceived) {
+    gifts.forEach(gift => {
+        const [giver, receiver] = gift.split(' ');
+        giftGiven[giver][receiver]++;
+        giftReceived[receiver][giver]++;
+    });
+
 }
+
+function calculateGiftBalance(friends, giftGiven, giftReceived) {
+    let giftBalance = {};
+    friends.forEach(friend => {
+        giftBalance[friend] = Object.values(giftReceived[friend]).reduce((a, b) => a + b, 0) - 
+                              Object.values(giftGiven[friend]).reduce((a, b) => a + b, 0);
+    });
+    console.log(giftBalance)
+    return giftBalance;
+}
+
+function predictNextMonthGifts(friends, giftGiven, giftReceived, giftBalance) {
+    let nextMonthGifts = {};
+    friends.forEach(friend => nextMonthGifts[friend] = 0);
+
+    friends.forEach(giver => {
+        friends.forEach(receiver => {
+            if (giver !== receiver) {
+                if (giftGiven[giver][receiver] > giftReceived[giver][receiver]) {
+                    nextMonthGifts[giver]++;
+                } else if (giftGiven[giver][receiver] === giftReceived[giver][receiver]) {
+                    let giverGiftBalance = Object.values(giftGiven[giver]).reduce((a, b) => a + b, 0) - 
+                                           Object.values(giftReceived[giver]).reduce((a, b) => a + b, 0);
+                    let receiverGiftBalance = Object.values(giftGiven[receiver]).reduce((a, b) => a + b, 0) - 
+                                              Object.values(giftReceived[receiver]).reduce((a, b) => a + b, 0);
+                    if (giverGiftBalance > receiverGiftBalance) {
+                        nextMonthGifts[giver]++;
+                    }
+                }
+            }
+        });
+    });
+    return nextMonthGifts;
+}
+
+function solution(friends, gifts) {
+    let { giftGiven, giftReceived } = initializeRecords(friends);
+    updateGiftRecords(gifts, giftGiven, giftReceived);
+    console.log(giftGiven)
+    let giftBalance = calculateGiftBalance(friends, giftGiven, giftReceived);
+
+    let nextMonthGifts = predictNextMonthGifts(friends, giftGiven, giftReceived, giftBalance);
+    return Math.max(...Object.values(nextMonthGifts));
+}
+
+let friendsExample1 = ["muzi", "ryan", "frodo", "neo"];
+let giftsExample1 = ["muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"];
+console.log(solution(friendsExample1, giftsExample1));
+
+let friendsExample2 = ["joy", "brad", "alessandro", "conan", "david"];
+let giftsExample2 = ["alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"];
+console.log(solution(friendsExample2, giftsExample2));
+
+let friendsExample3 = ["a", "b", "c"];
+let giftsExample3 = ["a b", "b a", "a c", "c a", "b c", "c b"];
