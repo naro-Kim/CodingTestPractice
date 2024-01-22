d01ed757071f9643f48e74397db467a2b926d788
naro_Kim <51940808+naro-Kim@users.noreply.github.com>
2024-01-22 09:36:26 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,13 +1,5 @@
 // 최대공약수
 const gcd = (a,b) => a%b === 0 ? b : gcd(b, a%b); 
 
-function solution(arr) { 
-    let g = gcd(arr[0], arr[1]);
-    let ans = Math.floor((arr[0] * arr[1])/g); 
-    
-    for(let i=2; i < arr.length; i++){
-        g = gcd(ans, arr[i]); 
-        ans = Math.floor((ans * arr[i]) / g) 
-    }
-    return ans;
-}
+// n개의 최소공배수는 최소공배수 누적
+const solution = (arr) => arr.reduce((a,b)=> a*b / gcd(a,b)); 
