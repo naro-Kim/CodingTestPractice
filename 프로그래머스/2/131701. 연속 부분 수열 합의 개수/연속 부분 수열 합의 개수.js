4464ac1109ae2fba5177525372892176883e5e52
naro_Kim <51940808+naro-Kim@users.noreply.github.com>
2024-01-25 10:55:29 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 103.64 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,12 +1,23 @@
 function solution(elements) {
-    const q = elements.concat(elements);
-    const set = new Set();
-    for (let i = 0; i < elements.length; i++) {
+    let s = new Set(); 
+    const len = elements.length;
+
+    for(let i=1; i<=len; i++){
+        // i = 부분 수열 사이즈
         let sum = 0;
-        for (let j = 0; j < elements.length; j++) {
-            sum += q[i + j];
-            set.add(sum);
-        }
+        let rear = 0;
+
+         // 원소가 하나인 부분 수열은 바로 합을 구해 더해준다 
+        for(let front=0; front<len; front++){  
+            if(front === 0){
+                while(rear < len) sum += elements[rear++];
+            } else { 
+                sum += elements[(front+i-1)%len];
+
+            }
+        s.add(sum);
+        } 
     }
-    return set.size;
+
+    return s.size
 }
